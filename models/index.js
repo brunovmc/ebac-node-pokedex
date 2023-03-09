953fd58ee7182fc91747c29b197bfa6cf2a12486
Bruno Vidigal <bruno.vidigal.coriolano@gmail.com>
2023-03-09 09:30:07 -0300

    adicionando arq seed e dotenv

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const PokemonSchema = require("./pokemon");
 const Pokemon = mongoose.model("Pokemon", PokemonSchema);
 
 const connect = () => {
-  mongoose.connect("mongodb://127.0.0.1:27017/pokedex");
+  mongoose.connect(process.env.MONGO_URL);
 };
 
 module.exports = {
--- routes/pokemons.js
+++ routes/pokemons.js
@@ -3,34 +3,32 @@ const express = require("express");
 const { Pokemon } = require("../models");
 const router = express.Router();
 
-router.get("/", (_, res) => {
-  Pokemon.find().then((pokemons) => {
-    pokemons.forEach((pokemon) => {
-      const dataIso = pokemon._id.getTimestamp().toISOString();
-      const dataInfo = dataIso.split("T")[0];
-      const dataEmPedacos = dataInfo.split("-");
-      pokemon.capturadoEm = `${dataEmPedacos[2]}/${dataEmPedacos[1]}/${dataEmPedacos[0]}`;
-    });
-    res.render("paginas/pokemons/index", {
-      pokemons: pokemons,
-    });
+router.get("/", async (_, res) => {
+  const pokemons = await Pokemon.find();
+  pokemons.forEach((pokemon) => {
+    const dataIso = pokemon._id.getTimestamp().toISOString();
+    const dataInfo = dataIso.split("T")[0];
+    const dataEmPedacos = dataInfo.split("-");
+    pokemon.capturadoEm = `${dataEmPedacos[2]}/${dataEmPedacos[1]}/${dataEmPedacos[0]}`;
+  });
+  res.render("paginas/pokemons/index", {
+    pokemons: pokemons,
   });
 });
 
-router.get("/:id", (req, res) => {
-  Pokemon.findOne({ _id: req.params.id })
-    .then((pokemon) => {
-      res.render("paginas/pokemons/show", {
-        pokemon,
-        message: req.query.message,
-      });
-    })
-    .catch((e) => {
-      res.status(404).render("paginas/erro", {
-        mensagem: "Pokemon nao encontrado!",
-        erro: {},
-      });
+router.get("/:id", async (req, res) => {
+  try {
+    const pokemon = await Pokemon.findOne({ _id: req.params.id });
+    res.render("paginas/pokemons/show", {
+      pokemon,
+      message: req.query.message,
     });
+  } catch (e) {
+    res.status(404).render("paginas/erro", {
+      mensagem: "Pokemon nao encontrado!",
+      erro: {},
+    });
+  }
 });
 
 module.exports = router;
